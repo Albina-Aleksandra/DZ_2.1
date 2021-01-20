1832e27d146dd800ba64451d4d6c9a878e87f87d
Albina <alninayakupova9@icloud.com>
2021-01-20 16:13:14 +0300

    Задание 2.1 выполнено. Программа работает.

--- Program.cs
+++ Program.cs
@@ -5,8 +5,19 @@ namespace DZ_2._1
     class Program
     {
         static void Main(string[] args)
+            
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Для определения средней температуры за сутки требуется ввести необходимые данные." +
+                $"Введите min температуру за сутки :") ; 
+                int min = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите максимальную температуру за сутки :");
+                int max = int.Parse(Console.ReadLine());
+                int temp = ((min + max) / 2);
+            Console.WriteLine($"Средняя температура за сутки : {temp}");
+         
+
+
         }
+        
     }
 }
